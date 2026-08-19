1de20d3bd8d099b1755d528eb4b2ecfab41da461
Willian Pena <willianoliveirapena10@gmail.com>
2026-08-19 01:32:56 -0300

    Set up turtle graphics for layout drawing
    Initialize turtle graphics for drawing layout.

---
+++ atv_dia_0209.py
@@ -0,0 +1,11 @@
+import turtle
+
+# Configura a tela
+screen = turtle.Screen()
+screen.title("Layout da Caixa de Pasta de Dente")
+screen.setup(width=800, height=600)
+
+# Cria a tartaruga para desenhar
+tr = turtle.Turtle()
+tr.speed(5)
+tr.pensize(2)
