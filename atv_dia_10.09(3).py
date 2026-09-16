c32220ca0b840b52acdf78309b78e1f11dae1885
Willian Pena <willianoliveirapena10@gmail.com>
2026-09-16 17:37:59 -0300

    Add Python script for calculating average grade

---
+++ atv_dia_10.09(3).py
@@ -0,0 +1,11 @@
+n1 = floap (input("n1:")
+n2 = floap (input("n2:")
+n3 = floap (input("n3:")
+medida = (n1+n2+n3) /3
+if media => 7.0:
+	print ("Aprovado")
+	print ("Media: {media: .if}")
+else
+	nota_exame = float (input("nota exame:"))
+	nova_media = (media + nota exame) /2
+if nova
